5a83d96e4a9c8aa938f846927088f132a6f5a0b6
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-07-28 02:13:08 -0800

    Yield

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Yield
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            foreach (string element in UseerCollection.Power()) 
+            {
+                Console.WriteLine(element) ;
+            }
+            
+            foreach (string element in UseerCollection.NoPower())
+            {
+                Console.WriteLine(element);
+            }
+            
+            foreach (int element in UseerCollection.InragerIenum())
+            {
+                Console.WriteLine(element);
+            }
+
+            foreach (object element in UseerCollection.Objejcts())
+            {
+                Console.WriteLine(element);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UseerCollection.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+
+namespace Yield
+{
+    internal class UseerCollection
+    {
+       public static  IEnumerable Power() 
+        {
+            yield return "Hello Word";
+            yield return "Hello Word";
+            yield return "Hello Word";
+            yield return "Hello Word";
+            yield return "Hello Word";
+        }
+
+        public static IEnumerable NoPower()
+        {
+            yield break;
+        }
+
+        public static IEnumerable InragerIenum()
+        {
+            yield return 5562;
+            yield return 62;
+            yield return 212;
+            yield return 51;
+            yield return 254;
+        }
+
+        public static IEnumerable Objejcts()
+        {
+            yield return 5562;
+            yield return "sad";
+            yield return "asda";
+            yield return false;
+            yield return 56.5;
+        }
+
+    }
+}
